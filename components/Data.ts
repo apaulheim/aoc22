03b15e21b41f0fb601c675dbad858a62fac5b56b
Annika Paulheim <annika.paulheim@technia.com>
2022-12-15 09:22:36 +0100

    day15 js

--- components/Data.ts
+++ components/Data.ts
@@ -22,6 +22,7 @@ export const solutions = [
   [Language.JS],
   [Language.JS],
   [Language.JS],
+  [Language.JS], // Day 15
 ];
 export const languageColors = [[89, 63, 78, 90], [44, 75, 78, 90], [
   21,
